0331a23d368c3bc8a256785f1b10458979122887
Shah rahman <rahman.shah@protonmail.ch>
2021-05-31 06:59:26 +0300

    hangman - second  step

--- hangman.py
+++ hangman.py
@@ -5,3 +5,11 @@ wordlist = ['greece', 'tokyo', 'olympic']
 chosen_word = random.choice(wordlist)
 
 print(chosen_word)
+
+guess_letter = input("Guess a litter in a chosen word :").lower()
+
+for letter in chosen_word:
+    if letter == guess_letter:
+        print("Right!")
+    else:
+        print("Wrong!")
