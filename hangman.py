6349ed7ebfeb2e183942475ed133f6c584f62f85
Shah rahman <rahman.shah@protonmail.ch>
2021-05-31 17:39:42 +0300

    hangman - third  step

--- hangman.py
+++ hangman.py
@@ -6,10 +6,17 @@ chosen_word = random.choice(wordlist)
 
 print(chosen_word)
 
+blank = []
+
+for item in range(len(chosen_word)):
+    blank += "_"
+
+print(blank)
+
 guess_letter = input("Guess a litter in a chosen word :").lower()
 
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess_letter:
-        print("Right!")
-    else:
-        print("Wrong!")
+        blank[position] = letter
+print(blank)
