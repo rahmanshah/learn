5046f0514ccc177bd4618d5c7a838b6cce5e3ae0
Shah rahman <rahman.shah@protonmail.ch>
2021-06-04 06:44:56 +0300

    Calculator step 2

--- calculator.py
+++ calculator.py
@@ -1,13 +1,17 @@
 #Calculator
 
 def add(n1,n2):
+    """ This function will return addition"""
     return n1 + n2
 
 def subtract(n1,n2):
+    """ This function will return subtraction"""
     return n1 - n2
 
 def multiply(n1,n2):
+    """ This function will return multiplication"""
     return n1 * n2
 
-def divide(n1,n2):
+def divide(n1, n2):
+    """ This function will return division"""
     return n1 / n2
